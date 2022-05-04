27f89f4c310f9f170270edd24084b69eb4cdbfc2
bugaringonzalo <bugaringonzalo@gmail.com>
2022-05-03 22:00:32 -0300

    First Commit

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const { Sequelize } = require('sequelize')
 const fs = require('fs')
 const path = require('path')
+const {DB_HOST, DB_PASSWORD, DB_USER} = process.env
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/repasopi`,
@@ -36,8 +37,11 @@ sequelize.models = Object.fromEntries(capsEntries)
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-
+const {Character, Episode} = sequelize.models
 // Aca vendrian las relaciones
+
+Character.belongsToMany ( Episode, {through: 'character_episode'} )
+Episode.belongsToMany ( Character, {through: 'character_episode'} )
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,30 @@
 const { Router } = require("express");
-
+const axios = require ('axios')
+const {Character, Episode} = require ('../db')
 const router = Router();
 
 // Configurar los routers
+
+const getApiInfo = async () => {
+    
+    const apiInfo = await axios.get(`https://rickandmortyapi.com/api/character`)
+    let allApi = await apiInfo.data.results.map((element) => {
+        return {
+            id: element.id,
+            name: element.id,
+            species: element.species,
+            origin: element.origin.name,
+            image: element.image,
+            episode: element.episode
+        }
+    })
+    return allApi
+}
+
+router.get('/characters', async (req,res,next) => {
+    let allCharacters = await getApiInfo();
+
+    return res.status(200).send(allCharacters)
+})
 
 module.exports = router;
